3d17cc40b3fc32d5b0adb4f3d48307f7ff84b46f
Juan García-Quismondo Fernández <juanitogarciaman@gmail.com>
2017-08-21 12:20:55 +0200

    First attempt
    Very basic attempt to make a HTTP GET request

---
+++ src/main/java/com/deccom/service/APIRestCallsService.java
@@ -0,0 +1,10 @@
+package com.deccom.service;
+
+public interface APIRestCallsService {
+
+	/**
+	 * Sends a HTTP GET request.
+	 */
+	void sendGet() throws Exception;
+
+}
---
+++ src/main/java/com/deccom/service/impl/APIRestCallsServiceImpl.java
@@ -0,0 +1,70 @@
+package com.deccom.service.impl;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.deccom.service.APIRestCallsService;
+
+public class APIRestCallsServiceImpl implements APIRestCallsService {
+
+	private final Logger log = LoggerFactory.getLogger(APIRestCallsServiceImpl.class);
+
+	public APIRestCallsServiceImpl() {
+
+	}
+
+	// private final String USER_AGENT = "Mozilla/5.0";
+	private final String USER_AGENT = "Chrome/60.0.3112.101";
+
+	public static void main(String[] args) throws Exception {
+		APIRestCallsServiceImpl http = new APIRestCallsServiceImpl();
+
+		System.out.println("Testing 1 - Send Http GET request");
+		http.sendGet();
+	}
+
+	// HTTP GET request
+	public void sendGet() throws Exception {
+
+		String url = "https://jsonplaceholder.typicode.com/users";
+		// String url = "https://jsonplaceholder.typicode.com/users/1";
+		// String url = "http://127.0.0.1:8080/books/";
+		// String url = "http://127.0.0.1:8080/books/1";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		// add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		// System.out.println("\nSending 'GET' request to URL : " + url);
+		log.debug("\nSending 'GET' request to URL : " + url);
+		// System.out.println("Response Code : " + responseCode);
+		log.debug("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		// print result
+		// System.out.println(response.toString());
+		log.debug(response.toString());
+
+	}
+
+}
