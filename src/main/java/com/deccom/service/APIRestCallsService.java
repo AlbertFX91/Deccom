1745960277c8af65ef719aff4a5fc89c8e0c4f09
Juan García-Quismondo Fernández <juanitogarciaman@gmail.com>
2017-08-23 12:51:49 +0200

    Time to refactor

--- src/main/java/com/deccom/service/APIRestCallsService.java
+++ src/main/java/com/deccom/service/APIRestCallsService.java
@@ -1,16 +1,25 @@
 package com.deccom.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.deccom.domain.Post;
+
 public interface APIRestCallsService {
 
 	/**
 	 * Sends a HTTP GET request to http://127.0.0.1:8080/books/.
+	 * 
+	 * @return the requested books
 	 */
-	void sendGetBooks() throws Exception;
+	List<Map<String, Object>> noMapping() throws Exception;
 
 	/**
 	 * Sends a HTTP GET request to
 	 * https://jsonplaceholder.typicode.com/posts?userId=1.
+	 * 
+	 * @return the requested posts
 	 */
-	void sendGetPosts() throws Exception;
+	List<Post> mapping() throws Exception;
 
 }
--- src/main/java/com/deccom/service/impl/APIRestCallsServiceImpl.java
+++ src/main/java/com/deccom/service/impl/APIRestCallsServiceImpl.java
@@ -4,18 +4,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import com.deccom.domain.Post;
 import com.deccom.service.APIRestCallsService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
+@Service
 public class APIRestCallsServiceImpl implements APIRestCallsService {
 
 	private final Logger log = LoggerFactory
@@ -25,21 +31,21 @@ public class APIRestCallsServiceImpl implements APIRestCallsService {
 
 	}
 
-	// private final String USER_AGENT = "Mozilla/5.0";
-	private final String USER_AGENT = "Chrome/60.0.3112.101";
-
 	public static void main(String[] args) throws Exception {
+
 		APIRestCallsServiceImpl http = new APIRestCallsServiceImpl();
 
 		System.out.println("Testing 1 - Send Http GET request");
-		http.sendGetBooks();
-		// http.sendGetPosts();
+		http.noMapping();
+
 	}
 
-	// HTTP GET request
-	public void sendGetBooks() throws Exception {
+	private final String USER_AGENT = "Chrome/60.0.3112.101";
 
-		String url = "http://127.0.0.1:8080/books/";
+	// HTTP GET request
+	public List<Map<String, Object>> noMapping() throws Exception {
+
+		String url = "https://jsonplaceholder.typicode.com/posts";
 
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -51,9 +57,7 @@ public class APIRestCallsServiceImpl implements APIRestCallsService {
 		con.setRequestProperty("User-Agent", USER_AGENT);
 
 		int responseCode = con.getResponseCode();
-		// System.out.println("\nSending 'GET' request to URL : " + url);
 		log.debug("\nSending 'GET' request to URL : " + url);
-		// System.out.println("Response Code : " + responseCode);
 		log.debug("Response Code : " + responseCode);
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(
@@ -66,20 +70,28 @@ public class APIRestCallsServiceImpl implements APIRestCallsService {
 		}
 		in.close();
 
-		// System.out.println(response.toString());
-		// log.debug(response.toString());
+		// print result
+		System.out.println(response.toString());
 
 		JSONArray jsonArray = new JSONArray(response.toString());
+		
+		List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+		ObjectMapper mapper = new ObjectMapper();
 
 		for (int i = 0; i < jsonArray.length(); i++) {
 			JSONObject finalObject = jsonArray.getJSONObject(i);
-			System.out.println("Book: " + finalObject.toString());
+			Map<String, Object> map = mapper.readValue(finalObject.toString(), new TypeReference<Map<String, String>>(){});
+			result.add(map);
 		}
+		
+		// This array is created from the response, and contains all the JSON
+		// objects to be returned
+		return result;
 
 	}
 
 	// HTTP GET request
-	public void sendGetPosts() throws Exception {
+	public List<Post> mapping() throws Exception {
 
 		String url = "https://jsonplaceholder.typicode.com/posts?userId=1";
 
@@ -93,9 +105,7 @@ public class APIRestCallsServiceImpl implements APIRestCallsService {
 		con.setRequestProperty("User-Agent", USER_AGENT);
 
 		int responseCode = con.getResponseCode();
-		// System.out.println("\nSending 'GET' request to URL : " + url);
 		log.debug("\nSending 'GET' request to URL : " + url);
-		// System.out.println("Response Code : " + responseCode);
 		log.debug("Response Code : " + responseCode);
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(
@@ -108,23 +118,30 @@ public class APIRestCallsServiceImpl implements APIRestCallsService {
 		}
 		in.close();
 
-		// System.out.println(response.toString());
-		// log.debug(response.toString());
+		log.debug(response.toString());
 
-		List<Post> posts = new LinkedList<Post>();
+		// This list will contain the posts to be returned when mapped
+		List<Post> result = new LinkedList<Post>();
 
+		// This array is created from the response, and contains all the JSON
+		// objects to be mapped
 		JSONArray jsonArray = new JSONArray(response.toString());
 
+		// This will be used for the parsing of the JSON objects
 		Gson gson = new Gson();
 		for (int i = 0; i < jsonArray.length(); i++) {
+			// Each JSON in the array is dealed with
 			JSONObject finalObject = jsonArray.getJSONObject(i);
+			// And each one of them is mapped into a Post object
 			Post post = gson.fromJson(finalObject.toString(), Post.class);
-			posts.add(post);
+			// Each post is added to the list
+			result.add(post);
 		}
 
-		// And this is how the mapped books look like
-		for (Post post : posts) {
-			System.out.println("User1 posts: " + post);
-		}
+		// Finally, the list with the mapped posts from the JSON objects is
+		// returned
+		return result;
+
 	}
+
 }
