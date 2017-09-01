b9ef77f21769b00b9e13d93b7362390d1c2edc5e
Juan García-Quismondo Fernández <juanitogarciaman@gmail.com>
2017-09-01 15:27:56 +0200

    New e2e test

--- src/test/javascript/e2e/operations/apirestcalls.spec.ts
+++ src/test/javascript/e2e/operations/apirestcalls.spec.ts
@@ -32,14 +32,27 @@ describe('API REST calls e2e test', () => {
         });
     });
 
-    it('should send a GET request', () => {
+    it('should send a GET request for an array', () => {
         element(by.css('[id="field_url"]')).sendKeys("https://jsonplaceholder.typicode.com/posts");
         element(by.css('[type="submit"]')).click();
 
         var table = element.all(by.css('[class="table-responsive"]'));
         var cells = table.all(by.tagName('td'));
 
-        expect(cells.count()).toBeGreaterThanOrEqual(100);
+        expect(cells.count()).toBeLessThanOrEqual(400);
+        expect(cells.count()).toBeGreaterThanOrEqual(400);
+    });
+
+    it('should send a GET request for a single JSON', () => {
+        element(by.css('[id="field_url"]')).clear();
+        element(by.css('[id="field_url"]')).sendKeys("https://jsonplaceholder.typicode.com/posts/1");
+        element(by.css('[type="submit"]')).click();
+
+        var container = element.all(by.css('[class="container"]'));
+        var cells = container.all(by.tagName('tr'));
+
+        expect(cells.count()).toBeLessThanOrEqual(8);
+        expect(cells.count()).toBeGreaterThanOrEqual(8);
     });
 
     afterAll(() => {
