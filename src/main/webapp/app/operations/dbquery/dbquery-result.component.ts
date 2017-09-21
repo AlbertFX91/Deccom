e0ea0260bb33bacfe6a218e4a2cc35fc66dfd44a
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2017-09-21 23:12:02 +0200

    Added the functionality of selecting an attribute

--- src/main/webapp/app/operations/dbquery/dbquery-result.component.ts
+++ src/main/webapp/app/operations/dbquery/dbquery-result.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Subscription } from 'rxjs/Rx';
 import { JhiEventManager, JhiParseLinks, JhiPaginationUtil, JhiLanguageService, JhiAlertService } from 'ng-jhipster';
-import { DBResponse } from './dbquery.model'
+import { DBResponse, DBField } from './dbquery.model'
 import { ITEMS_PER_PAGE, Principal, ResponseWrapper } from '../../shared';
 
 @Component({
     selector: 'jhi-dbquery-list',
-    templateUrl: './dbquery-result.component.html'
+    templateUrl: './dbquery-result.component.html',
+    styleUrls: [
+        'dbquery-result.css'
+    ],
 })
 export class DBQueryResultComponent implements OnInit, OnDestroy {
 
     @Input()
     dbResponse: DBResponse;
+
+    fieldSelected: any = {};
 
     constructor(
     ) {}
@@ -22,5 +27,15 @@ export class DBQueryResultComponent implements OnInit, OnDestroy {
 
     fields() {
         return this.dbResponse.metadata.fields;
+    }
+
+    onFieldClick(index: number, field: DBField) {
+        this.fieldSelected.index = index;
+        this.fieldSelected.field = field;
+    }
+
+    isFieldSelected(index: number, field: DBField) {
+        return this.fieldSelected.index ===index &&
+            this.fieldSelected.field === field;
     }
 }
