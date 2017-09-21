60feee98777eeece8e7241cf147693cf29f5d079
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2017-09-21 22:43:12 +0200

    Refactored the frontend with the new dbresult data structure

--- src/main/webapp/app/operations/dbquery/dbquery-result.component.ts
+++ src/main/webapp/app/operations/dbquery/dbquery-result.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Subscription } from 'rxjs/Rx';
 import { JhiEventManager, JhiParseLinks, JhiPaginationUtil, JhiLanguageService, JhiAlertService } from 'ng-jhipster';
-
+import { DBResponse } from './dbquery.model'
 import { ITEMS_PER_PAGE, Principal, ResponseWrapper } from '../../shared';
 
 @Component({
@@ -11,8 +11,7 @@ import { ITEMS_PER_PAGE, Principal, ResponseWrapper } from '../../shared';
 export class DBQueryResultComponent implements OnInit, OnDestroy {
 
     @Input()
-    queryResult: any;
-    predicate: any;
+    dbResponse: DBResponse;
 
     constructor(
     ) {}
@@ -21,21 +20,7 @@ export class DBQueryResultComponent implements OnInit, OnDestroy {
 
     ngOnDestroy() {}
 
-    list() {
-        return this.queryResult.length > 1;
-    }
-
-    detail() {
-        return this.queryResult.length === undefined;
-    }
-
-    properties() {
-        // If its a list, we get one element from the list and we get all the keys
-        // After get it, we sort it to get always the same order of the keys.
-        if (this.list()) {
-            return Object.keys(this.queryResult[0]).sort();
-        } else {
-            return Object.keys(this.queryResult).sort();
-        }
+    fields() {
+        return this.dbResponse.metadata.fields;
     }
 }
