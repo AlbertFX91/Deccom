f3aa1c39e95c3ec20183678c8488ff3b4470b466
Juan García-Quismondo Fernández <juanitogarciaman@gmail.com>
2017-10-28 13:38:43 +0200

    Working on displaying image from model

--- src/main/webapp/app/operations/cv/cv.model.ts
+++ src/main/webapp/app/operations/cv/cv.model.ts
@@ -1,7 +1,7 @@
 export class CVCard {
     constructor(
         public id?: string,
-        public logo_url?: string,
+        public logo?: string,
         public name?: string,
         public value?: string,
         public entries?: number,
