ebaf8ee2c7b508312820709959e5cf6956b89092
Juan García-Quismondo Fernández <juanitogarciaman@gmail.com>
2017-11-17 13:55:10 +0100

    Working on animation for toggle

--- src/main/webapp/app/operations/cv/cv-card.component.ts
+++ src/main/webapp/app/operations/cv/cv-card.component.ts
@@ -2,11 +2,26 @@ import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CVCard } from './cv.model';
 import { CVService } from './cv.service';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
     selector: 'jhi-cv-card',
     templateUrl: './cv-card.component.html',
-    styleUrls: ['./cv-card.component.css']
+    styleUrls: ['./cv-card.component.css'],
+    animations: [
+        trigger('collapseAnimation', [
+            state('isCollapsed', style({
+                transform: 'scale(1)'
+            })),
+            state('!isCollapsed', style({
+                transform: 'scale(1.2)'
+            })),
+            transition('isCollapsed <=> !isCollapsed', animate('300ms ease-in', keyframes([
+                style({opacity: 0, offset: 0}),
+                style({opacity: 1, offset: 1})
+            ])))
+        ])
+    ]
 })
 export class CVCardComponent implements OnInit, OnDestroy {
 
