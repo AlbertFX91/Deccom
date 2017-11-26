212199d1cb7356ca3cca96690431948c19fb9a4f
Juan García-Quismondo Fernández <juanitogarciaman@gmail.com>
2017-11-26 13:06:20 +0100

    Enabling configuration properties in RESTUtil.java

--- src/main/java/com/deccom/service/impl/util/RESTUtil.java
+++ src/main/java/com/deccom/service/impl/util/RESTUtil.java
@@ -19,11 +19,13 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.deccom.config.ApplicationProperties;
 import com.deccom.service.impl.RESTServiceImpl;
 import com.jayway.jsonpath.JsonPath;
 
+@EnableConfigurationProperties({ ApplicationProperties.class })
 public class RESTUtil {
 
 	private static final Logger log = LoggerFactory
