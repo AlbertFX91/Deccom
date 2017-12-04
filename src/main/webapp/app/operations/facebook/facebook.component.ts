38abf1d18a549e9422f3eb228db4e7866eab2570
Juan García-Quismondo Fernández <juanitogarciaman@gmail.com>
2017-12-04 18:33:01 +0100

    Finally retrieving data, still need to add the right parameters

--- src/main/webapp/app/operations/facebook/facebook.component.ts
+++ src/main/webapp/app/operations/facebook/facebook.component.ts
@@ -37,7 +37,7 @@ import { PaginationConfig } from '../../blocks/config/uib-pagination.config';
         };
         this.data = [];
         this.path = '';
-        this.url = 'https://graph.facebook.com/v2.11/me/friends';
+        this.url = 'https://graph.facebook.com/v2.11/131714104182428';
     }
 
     loadPage(page) {
