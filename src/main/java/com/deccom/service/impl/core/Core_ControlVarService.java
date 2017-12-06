5b2144a0ea24a7ba900f1ab919ebf719e41ed5d3
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2017-12-06 14:51:30 +0100

    ExecuteMonitorize refactored

--- src/main/java/com/deccom/service/impl/core/Core_ControlVarService.java
+++ src/main/java/com/deccom/service/impl/core/Core_ControlVarService.java
@@ -73,34 +73,17 @@ public class Core_ControlVarService {
     	}
     }   
     public void executeMonitorize(Core_ControlVar controlVar) {
-    	Core_Connection connection = controlVar.getConnection();
-    	Object wrapper;
-    	if(connection.getClass().isAnnotationPresent(Core_Extractor.class)) {
-        	Annotation annotation = connection.getClass().getAnnotation(Core_Extractor.class);
-        	Core_Extractor core_extractor = (Core_Extractor) annotation;
-        	try {
-				wrapper = core_extractor.value().newInstance();
-			} catch (InstantiationException | IllegalAccessException e) {
-				// TODO This is when the class its an interface, an abstract class, or its private.
-				// So, if its happens, its a programming error. It will throw an exception	
-				e.printStackTrace();
-				wrapper = null;
-    			System.out.println("Error. Cannot instanciate the extractor");
-			}
-    		
-    		Map<String, Core_Connection> fields = new HashMap<>();
-    		fields.put("dataConnection", connection);
-    		if(wrapper instanceof Core_DataExtractor) {
-    			Core_DataExtractor dataExtractor = (Core_DataExtractor) wrapper;
-    			propertiesInjection(dataExtractor, fields);
-    			String value = dataExtractor.getData();
-    			addEntry(controlVar, value);
-    			log.debug("Data extracted ["+controlVar.getName()+"]: "+value);
-    		}else {
-    			// TODO This is when the class in @Extractor does not extends from Core_DataExtractor, but its controlled by the Core_Extractor interface.
-    			// If its happens, but its unlikely, it will throw an exception
-    			System.out.println("Error. Wrapper is not an instance of Core_DataExtractor");
-    		}
+    	Core_DataExtractor dataExtractor;
+    	Core_Connection connection;
+    	String value;
+    	
+    	connection = controlVar.getConnection();
+    	if(hasExtractor(connection)) {
+    		dataExtractor = getExtractorByConnection(connection);
+    		dataExtractor = injectConnectionInExtractor(dataExtractor, connection);
+    		value = dataExtractor.getData();
+    		addEntry(controlVar, value);
+    		log.debug("Data extracted ["+controlVar.getName()+"]: "+value);
 		}else {
 			// TODO This is when a class has not the annotation @Extractor implemented. It'll throw an exception
 			System.out.println("Error. Connection has not the annotation @Extractor");
@@ -129,5 +112,36 @@ public class Core_ControlVarService {
 				throw new IllegalStateException(e);
 			}
 		}
+	}
+	private Boolean hasExtractor(Core_Connection connection) {
+		return connection.getClass().isAnnotationPresent(Core_Extractor.class);
+	}
+	private Core_DataExtractor injectConnectionInExtractor(Core_DataExtractor extractor, Core_Connection connection) {
+		Map<String, Core_Connection> fields = new HashMap<>();
+		fields.put("dataConnection", connection);
+		propertiesInjection(extractor, fields);
+		return extractor;
+	}
+	private Core_DataExtractor getExtractorByConnection(Core_Connection connection) {
+		Object wrapper;
+		Annotation annotation = connection.getClass().getAnnotation(Core_Extractor.class);
+	    Core_Extractor core_extractor = (Core_Extractor) annotation;
+	    try {
+	    	wrapper = core_extractor.value().newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			// TODO This is when the class its an interface, an abstract class, or its private.
+			// So, if its happens, its a programming error. It will throw an exception	
+			e.printStackTrace();
+	    	System.out.println("Error. Cannot instanciate the extractor");
+	    	return null;
+		}
+	    if(wrapper instanceof Core_DataExtractor) {
+	    	return (Core_DataExtractor) wrapper;
+	    }else {
+	    	// TODO This is when the class in @Extractor does not extends from Core_DataExtractor, but its controlled by the Core_Extractor interface.
+	    	// If its happens, but its unlikely, it will throw an exception
+	    	System.out.println("Error. Wrapper is not an instance of Core_DataExtractor");
+	    	return null;
+	    }
 	}
 }
