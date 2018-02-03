6559704856a81ab95bfbd47d23a3c0a5c05c9032
Juan García-Quismondo Fernández <juanitogarciaman@gmail.com>
2018-02-03 17:24:08 +0100

    feat(core): adding frequency to ControlVariable #280

--- src/main/java/com/deccom/newcore/domain/ControlVariable.java
+++ src/main/java/com/deccom/newcore/domain/ControlVariable.java
@@ -21,6 +21,8 @@ public class ControlVariable {
 	@NotNull
 	private LocalDateTime creationMoment;
 	@NotNull
+	private Integer frequency;
+	@NotNull
 	private List<ControlVariableEntry> controlVarEntries;
 	@NotNull
 	private ControlVariableExtractor extractor;
@@ -57,11 +59,20 @@ public class ControlVariable {
 		this.creationMoment = creationMoment;
 	}
 
+	public Integer getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(Integer frequency) {
+		this.frequency = frequency;
+	}
+
 	public List<ControlVariableEntry> getControlVarEntries() {
 		return controlVarEntries;
 	}
 
-	public void setControlVarEntries(List<ControlVariableEntry> controlVarEntries) {
+	public void setControlVarEntries(
+			List<ControlVariableEntry> controlVarEntries) {
 		this.controlVarEntries = controlVarEntries;
 	}
 
@@ -75,18 +86,29 @@ public class ControlVariable {
 
 	@Override
 	public String toString() {
-		return "ControlVar [id=" + id + ", name=" + name + ", creationMoment="
-				+ creationMoment + "]";
+		return "ControlVariable [id=" + id + ", name=" + name + ", status="
+				+ status + ", creationMoment=" + creationMoment
+				+ ", frequency=" + frequency + ", controlVarEntries="
+				+ controlVarEntries + ", extractor=" + extractor + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime
+				* result
+				+ ((controlVarEntries == null) ? 0 : controlVarEntries
+						.hashCode());
 		result = prime * result
 				+ ((creationMoment == null) ? 0 : creationMoment.hashCode());
+		result = prime * result
+				+ ((extractor == null) ? 0 : extractor.hashCode());
+		result = prime * result
+				+ ((frequency == null) ? 0 : frequency.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
 		return result;
 	}
 
@@ -99,10 +121,25 @@ public class ControlVariable {
 		if (getClass() != obj.getClass())
 			return false;
 		ControlVariable other = (ControlVariable) obj;
+		if (controlVarEntries == null) {
+			if (other.controlVarEntries != null)
+				return false;
+		} else if (!controlVarEntries.equals(other.controlVarEntries))
+			return false;
 		if (creationMoment == null) {
 			if (other.creationMoment != null)
 				return false;
 		} else if (!creationMoment.equals(other.creationMoment))
+			return false;
+		if (extractor == null) {
+			if (other.extractor != null)
+				return false;
+		} else if (!extractor.equals(other.extractor))
+			return false;
+		if (frequency == null) {
+			if (other.frequency != null)
+				return false;
+		} else if (!frequency.equals(other.frequency))
 			return false;
 		if (id == null) {
 			if (other.id != null)
@@ -113,6 +150,8 @@ public class ControlVariable {
 			if (other.name != null)
 				return false;
 		} else if (!name.equals(other.name))
+			return false;
+		if (status != other.status)
 			return false;
 		return true;
 	}
