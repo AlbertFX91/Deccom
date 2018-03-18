a5b973c493c2a4d24c1638a00dbcdfb0c813a8ca
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2018-03-18 23:38:33 +0100

    feat(fields-enriched): updated extractors with @DeccomField (#321)

--- src/main/java/com/deccom/domain/core/fields/DeccomField.java
+++ src/main/java/com/deccom/domain/core/fields/DeccomField.java
@@ -8,8 +8,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME) // The annotation is retained in runtime
 @Target(ElementType.FIELD)
 public @interface DeccomField {
-	
+
 	InputType type() default InputType.TEXT;
-	
+
 	String component() default "";
 }
