30d87c1a7b24d020cfca1abcac04618429e1d17e
Juan García-Quismondo Fernández <juanitogarciaman@gmail.com>
2018-04-11 19:01:07 +0200

    feat(events): chart working with auxiliar data #329

---
+++ src/main/java/com/deccom/domain/core/extractor/RandomExtractor.java
@@ -0,0 +1,35 @@
+package com.deccom.domain.core.extractor;
+
+import javax.validation.constraints.NotNull;
+
+import com.deccom.domain.core.CVStyle;
+
+public class RandomExtractor implements ControlVariableExtractor {
+
+	@NotNull
+	private CVStyle style;
+
+	public RandomExtractor() {
+		style = CVStyle.create("Random Extractor", "fa fa-asterisk", "#FF0000", "#FFFFFF");
+	}
+
+	@Override
+	public Integer getData() {
+		return (int) (Math.random() * 100);
+	}
+
+	@Override
+	public CVStyle getStyle() {
+		return style;
+	}
+
+	public void setStyle(CVStyle style) {
+		this.style = style;
+	}
+
+	@Override
+	public String toString() {
+		return "RandomExtractor []";
+	}
+
+}
